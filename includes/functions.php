124a19e9b4a47538706a85e6902d5c4f72fadc8a
HumanBooster <bclerc@humanbooster.com>
2015-03-24 15:46:16 +0100

    Déplacement des fonctions underscore<>camelCase dans functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,4 +1,51 @@
 <?php
+
 /**
- * Who needs function when you have classes ?
+ * Who needs functions when you have classes ?
  */
+
+/**
+ * Underscore to camelCase
+ * 
+ * @param string $str String to convert
+ * @return string Converted string to camelCase
+ */
+function us2cc($str) {
+// si la chaine ne contient pas de "_", on la renvoie telle quelle
+    if (strpos($str, "_") === false)
+        return $str;
+
+// sinon on explose la chaine dans un tableau
+// autour du délimiteur "_"
+    $words = explode("_", $str);
+
+// le compteur permet de différencier le premier mot (0)
+    $count = 0;
+// puis on concatène en camelCase
+    foreach ($words as $word) {
+// premier mot ) prendre tel quel
+        if ($count == 0)
+            $result = $word;
+// mots suivants à capitaliser
+        else
+            $result .= ucfirst($word);
+        $count++;
+    }
+    return $result;
+}
+
+/**
+ * CamelCase to Underscore
+ * 
+ * @param string $str camelCase string to convert
+ * @return string string converted to underscore
+ */
+function cc2us($str) {
+// this is clearly black magic !
+    preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $str, $matches);
+    $ret = $matches[0];
+    foreach ($ret as &$match) {
+        $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+    }
+    return implode('_', $ret);
+}
--- model/Entity.php
+++ model/Entity.php
@@ -36,7 +36,7 @@ class Entity {
                 //echo "\n Found key " . $key;
             } else {
                 // on convertis la clé vers du camelCase
-                $camelKey = $this->us2cc($key);
+                $camelKey = us2cc($key);
                 //echo "\n".$key ." => " .$camelKey;
                 if (property_exists(get_class($this), $camelKey)) {
                     // si on trouve, on lui assigne la valeur
@@ -63,55 +63,9 @@ class Entity {
             // on saute l'id, à moins que putId soit vrai
             if (($key!="id")||($putId))
                 // on convertit la clé en underscore pour stocker la valeur
-                $result[$this->cc2us($key)] = $value;
+                $result[cc2us($key)] = $value;
         }
         return $result;
-    }
-
-    /**
-     * Underscore to camelCase
-     * 
-     * @param string $str String to convert
-     * @return string Converted string to camelCase
-     */
-    public static function us2cc($str) {
-        // si la chaine ne contient pas de "_", on la renvoie telle quelle
-        if (strpos($str, "_") === false)
-            return $str;
-
-        // sinon on explose la chaine dans un tableau
-        // autour du délimiteur "_"
-        $words = explode("_", $str);
-
-        // le compteur permet de différencier le premier mot (0)
-        $count = 0;
-        // puis on concatène en camelCase
-        foreach ($words as $word) {
-            // premier mot ) prendre tel quel
-            if ($count == 0)
-                $result = $word;
-            // mots suivants à capitaliser
-            else
-                $result .= ucfirst($word);
-            $count++;
-        }
-        return $result;
-    }
-
-    /**
-     * CamelCase to Underscore
-     * 
-     * @param string $str camelCase string to convert
-     * @return string string converted to underscore
-     */
-    public static function cc2us($str) {
-        // this is clearly black magic !
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $str, $matches);
-        $ret = $matches[0];
-        foreach ($ret as &$match) {
-            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
-        }
-        return implode('_', $ret);
     }
 
 }
